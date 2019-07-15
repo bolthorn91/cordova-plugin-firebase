5b8a1c5b0073d7bbc66a644cc062c1393af52fab
unknown <bolthorn91@gmail.com>
2019-07-15 18:58:35 +0100

    te pasas

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -17,7 +17,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
